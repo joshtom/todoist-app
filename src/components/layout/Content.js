ac09e9eef7405bdf2aa814e7481c81b8afde87eb
Joshua <olajidejoshua4real@gmail.com>
2019-09-03 12:40:07 +0100

    Firebase file added

---
+++ src/components/layout/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Sidebar } from './Sidebar.js'
+
+export const Content = () => {
+    return (
+        <section>
+            <Sidebar />
+        </section>
+    )
+}
